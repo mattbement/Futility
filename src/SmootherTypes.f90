c5ce1ba7c56997305e3075ef4abf535c778a472c
Ben C. Yee <bcyee@umich.edu>
2017-09-22 15:54:17 -0400

    Define init and clear for SmootherType_PETSc_RBBJ
    Also create unit tests for them

--- src/SmootherTypes.f90
+++ src/SmootherTypes.f90
@@ -7,7 +7,7 @@
 ! can be found in LICENSE.txt in the head directory of this repository.        !
 !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++!
 !> @brief Module provides a smoother type for smoothing a system of equations.
-!>        It is intended to support multigrid linear solvers.
+!>        It is intended to support multigrid linear smoothers.
 !>
 !> @par Module Dependencies
 !>  - @ref IntrType "IntrType": @copybrief IntrType
@@ -45,19 +45,26 @@ MODULE SmootherTypes
   USE IOUtil
   IMPLICIT NONE
 
+#ifdef FUTILITY_HAVE_PETSC
+#include <finclude/petsc.h>
+!petscisdef.h defines the keyword IS, and it needs to be reset
+#undef IS
+#endif
+
   PRIVATE
 !
 ! List of public members
   PUBLIC :: eSmootherType
   PUBLIC :: SmootherType_Base
+  PUBLIC :: SmootherType_PETSc
+  PUBLIC :: SmootherType_PETSc_RBBJ
 
   !> set enumeration scheme for TPLs
   INTEGER(SIK),PARAMETER,PUBLIC :: PETSC=0,NATIVE=4
-  !> Enumeration for solver options
+  !> Enumeration for smoother options
   INTEGER(SIK),PARAMETER,PUBLIC :: SOR=0
 
-
-  !> @brief the base linear solver type
+  !> @brief the base linear smoother type
   TYPE,ABSTRACT :: SmootherType_Base
     !> Initialization status
     LOGICAL(SBK) :: isInit=.FALSE.
@@ -66,7 +73,7 @@ MODULE SmootherTypes
     !> Integer flag for the solution methodology desired
     INTEGER(SIK) :: TPLType=-1
     !> Pointer to an MPI parallel environment
-    CLASS(MPI_EnvType),POINTER :: MPIparallelEnv
+    TYPE(MPI_EnvType) :: MPIparallelEnv
     !> Has initial guess?
     LOGICAL(SBK) :: hasX0=.FALSE.
     !> Current local residual norm
@@ -77,24 +84,180 @@ MODULE SmootherTypes
   !
   !List of Type Bound Procedures
     CONTAINS
-      !> Deferred routine for initializing the linear solver system
-      PROCEDURE(smoother_sub_absintfc),DEFERRED,PASS :: init
-      !> Deferred routine for clearing the linear solver
+      !> Deferred routine for clearing the linear smoother
       PROCEDURE(smoother_sub_absintfc),DEFERRED,PASS :: clear
       !> Deferred routine for applying a smoother step to the linear system
       PROCEDURE(smoother_sub_absintfc),DEFERRED,PASS :: smooth
   ENDTYPE SmootherType_Base
 
+  TYPE,ABSTRACT,EXTENDS(SmootherType_Base) :: SmootherType_PETSc
+#ifdef FUTILITY_HAVE_PETSC
+    !> Pointer to PETSc KSP object, should type KSPRICHARDSON
+    KSP :: ksp
+    !> Pointer to PETSc PC object corresponding to ksp, should type PCSHELL
+    PC :: pc
+#else
+    !> Dummy attribute to make sure Futility compiles when PETSc is not present
+    INTEGER(SIK) :: ksp=-1_SIK
+    !> Dummy attribute to make sure Futility compiles when PETSc is not present
+    INTEGER(SIK) :: pc=-1_SIK
+#endif
+
+  !
+  !List of Type Bound Procedures
+    CONTAINS
+      !> Deferred routine for initializing the linear smoother system
+      PROCEDURE(smoother_petsc_sub_absintfc),DEFERRED,PASS :: init
+  ENDTYPE SmootherType_PETSc
+
+  !Red black block Jacobi smoother:
+  TYPE,EXTENDS(SmootherType_PETSc) :: SmootherType_PETSc_RBBJ
+    !> Number of local red blocks: (swept over first)
+    INTEGER(SIK) :: num_red=-1_SIK
+    !> Number of local black blocks: (swept over second)
+    INTEGER(SIK) :: num_black=-1_SIK
+    !> Starting local block index:
+    INTEGER(SIK) :: istt=-1_SIK
+    !> End local block index:
+    INTEGER(SIK) :: istp=-1_SIK
+    !Note that we should have num_red+num_black=istp-istt+1
+    !> Logical array indicating whether an index is red (istt:istp)
+    LOGICAL(SBK),ALLOCATABLE :: is_red(:)
+    !> Block size:
+    !>  The matrix rows owned locally should be 
+    !>    (istt-1)*blk_size+1:istp*blk_size
+    INTEGER(SIK) :: blk_size=-1_SIK
+
+  !
+  !List of Type Bound Procedures
+    CONTAINS
+      !> @copybrief SmootherTypes::init_SmootherType_PETSc_RBBJ
+      !> @copydetails SmootherTypes::init_SmootherType_PETSc_RBBJ
+      PROCEDURE,PASS :: init => init_SmootherType_PETSc_RBBJ
+      !> @copybrief SmootherTypes::clear_SmootherType_PETSc_RBBJ
+      !> @copydetails SmootherTypes::clear_SmootherType_PETSc_RBBJ
+      PROCEDURE,PASS :: clear => clear_SmootherType_PETSc_RBBJ
+      !> @copybrief SmootherTypes::smooth_SmootherType_PETSc_RBBJ
+      !> @copydetails SmootherTypes::smooth_SmootherType_PETSc_RBBJ
+      PROCEDURE,PASS :: smooth => smooth_SmootherType_PETSc_RBBJ
+  ENDTYPE SmootherType_PETSc_RBBJ
+
   !> Exception Handler for use in SmootherTypes
   TYPE(ExceptionHandlerType),SAVE :: eSmootherType
 
-  !> Explicitly defines the interface for the clear and solve routines
+  !> Explicitly defines the interface for the clear, and solve routines
   ABSTRACT INTERFACE
-    SUBROUTINE smoother_sub_absintfc(solver)
+    SUBROUTINE smoother_sub_absintfc(smoother)
       IMPORT :: SmootherType_Base
-      CLASS(SmootherType_Base),INTENT(INOUT) :: solver
+      CLASS(SmootherType_Base),INTENT(INOUT) :: smoother
     ENDSUBROUTINE smoother_sub_absintfc
   ENDINTERFACE
 
+  !> Explicitly defines the interface for the init routine for PETSc smoothers
+  ABSTRACT INTERFACE
+    SUBROUTINE smoother_petsc_sub_absintfc(smoother,ksp,params)
+      IMPORT :: SmootherType_PETSc
+      IMPORT :: ParamType
+      CLASS(SmootherType_PETSc),INTENT(INOUT) :: smoother
+      TYPE(ParamType),INTENT(IN) :: params
+#ifdef FUTILITY_HAVE_PETSC
+      KSP,INTENT(INOUT) :: ksp
+#else
+      INTEGER(SIK),INTENT(INOUT) :: ksp
+#endif
+    ENDSUBROUTINE smoother_petsc_sub_absintfc
+  ENDINTERFACE
+
+  !> Name of module
+  CHARACTER(LEN=*),PARAMETER :: modName='SMOOTHERTYPES'
+!
+!------------------------------------------------------------------------------
+  CONTAINS
+!
+!-------------------------------------------------------------------------------
+!> @brief Initializes the SmootherType for an RBBJ PETSc smoother
+!> @param smoother The smoother object to act on
+!>
+    SUBROUTINE init_SmootherType_PETSc_RBBJ(smoother,ksp,params)
+      CHARACTER(LEN=*),PARAMETER :: myName='init_SmootherType_PETSc_RBBJ'
+      CLASS(SmootherType_PETSc_RBBJ),INTENT(INOUT) :: smoother
+      TYPE(ParamType),INTENT(IN) :: params
+      LOGICAL(SBK) :: tmpbool
+      INTEGER(SIK) :: MPI_Comm_ID
+#ifdef FUTILITY_HAVE_PETSC
+      KSP,INTENT(INOUT) :: ksp
+      PC :: pc
+      PetscErrorCode :: iperr
+#else
+      INTEGER(SIK),INTENT(INOUT) :: ksp
+      INTEGER(SIK) :: pc
+
+      IF(.NOT. tmpbool) &
+        CALL eSmootherType%raiseError(modName//"::"//myName//" - "// &
+            "This type should only be used with PETSc enabled!")
+#endif
+      !Check parameter list:
+      tmpbool=params%has('SmootherType->num_red') .AND. &
+              params%has('SmootherType->num_black') .AND. &
+              params%has('SmootherType->istt') .AND. &
+              params%has('SmootherType->istp') .AND. &
+              params%has('SmootherType->blk_size') .AND. &
+              params%has('SmootherType->MPI_Comm_ID')
+      IF(.NOT. tmpbool) &
+        CALL eSmootherType%raiseError(modName//"::"//myName//" - "// &
+            "Missing a parameter from the parameter list!")
+
+      !Extract param list info:
+      CALL params%get('SmootherType->istt',smoother%istt)
+      CALL params%get('SmootherType->istp',smoother%istp)
+      ALLOCATE(smoother%is_red(smoother%istt:smoother%istp))
+      CALL params%get('SmootherType->is_red',smoother%is_red)
+      CALL params%get('SmootherType->blk_size',smoother%blk_size)
+      CALL params%get('SmootherType->MPI_Comm_ID',MPI_Comm_ID)
+
+      !Count the # of red/black cells:
+      smoother%num_red=COUNT(smoother%is_red)
+      smoother%num_black=smoother%istp-smoother%istt-smoother%num_red+1
+
+      MPI_Comm_ID=-1_SIK
+      IF(MPI_Comm_ID /= -1_SIK) THEN
+        CALL smoother%MPIparallelEnv%init(MPI_Comm_ID)
+      ELSE
+        CALL smoother%MPIparallelEnv%init(MPI_COMM_WORLD)
+      ENDIF
+
+      smoother%ksp=ksp
+#ifdef FUTILITY_HAVE_PETSC
+      CALL KSPSetType(smoother%ksp,KSPRICHARDSON,iperr)
+      CALL KSPGetPC(smoother%ksp,smoother%pc,iperr)
+      CALL PCSetType(smoother%pc,PCSHELL,iperr)
+#endif
+      smoother%isInit=.TRUE.
+
+    ENDSUBROUTINE init_SmootherType_PETSc_RBBJ
+!
+!-------------------------------------------------------------------------------
+!> @brief Performs a sweep over the red cells, and then the black cells
+!> @param smoother The smoother object to act on
+!>
+    SUBROUTINE smooth_SmootherType_PETSc_RBBJ(smoother)
+      CHARACTER(LEN=*),PARAMETER :: myName='smooth_SmootherType_PETSc_RBBJ'
+      CLASS(SmootherType_PETSc_RBBJ),INTENT(INOUT) :: smoother
+    ENDSUBROUTINE smooth_SmootherType_PETSc_RBBJ
+
+!
+!-------------------------------------------------------------------------------
+!> @brief Clears the SmootehrType for an RBBJ PETSc smoother
+!> @param smoother The smoother object to act on
+!>
+    SUBROUTINE clear_SmootherType_PETSc_RBBJ(smoother)
+      CHARACTER(LEN=*),PARAMETER :: myName='clear_SmootherType_PETSc_RBBJ'
+      CLASS(SmootherType_PETSc_RBBJ),INTENT(INOUT) :: smoother
+
+      IF(ALLOCATED(smoother%is_red)) DEALLOCATE(smoother%is_red)
+      CALL smoother%MPIparallelEnv%clear()
+      smoother%isInit=.FALSE.
+
+    ENDSUBROUTINE clear_SmootherType_PETSc_RBBJ
 !
 ENDMODULE SmootherTypes
--- unit_tests/testSmoother/testSmoother.f90
+++ unit_tests/testSmoother/testSmoother.f90
@@ -24,7 +24,6 @@ PROGRAM testSmoother
 
 #ifdef FUTILITY_HAVE_PETSC
 #include <finclude/petsc.h>
-#include <petscversion.h>
 #undef IS
   PetscErrorCode  :: ierr
 
@@ -41,8 +40,10 @@ PROGRAM testSmoother
   CALL eParams%addSurrogate(e)
   CALL eSmootherType%addSurrogate(e)
 
-  CREATE_TEST('Test Smoothers')
-
+  CREATE_TEST('Test SmootherType_PETSc_RBBJ')
+  REGISTER_SUBTEST('testInit',testInit_PETSc_RBBJ)
+  REGISTER_SUBTEST('testClear',testClear_PETSc_RBBJ)
+  REGISTER_SUBTEST('testSmooth',testSmooth_PETSc_RBBJ)
   FINALIZE_TEST()
 
 #ifdef FUTILITY_HAVE_PETSC
@@ -52,10 +53,172 @@ PROGRAM testSmoother
 #endif
 !
 !===============================================================================
-CONTAINS
+  CONTAINS
 !
 !-------------------------------------------------------------------------------
-    SUBROUTINE testClear()
-    ENDSUBROUTINE testClear
+    SUBROUTINE testClear_PETSc_RBBJ()
+      TYPE(SmootherType_PETSc_RBBJ) :: smoother
+
+      ALLOCATE(smoother%is_red(500))
+      smoother%MPIparallelEnv=mpiTestEnv
+
+      CALL smoother%clear()
+
+      ASSERT(.NOT.ALLOCATED(smoother%is_red),'Smoother%isred deallocated')
+      ASSERT(.NOT. smoother%MPIparallelEnv%isInit(),'Smoother parenv not initialized')
+      ASSERT(.NOT. smoother%isInit,'Smoother not initialized')
+    ENDSUBROUTINE testClear_PETSc_RBBJ
+!
+!-------------------------------------------------------------------------------
+    SUBROUTINE testInit_PETSc_RBBJ()
+      TYPE(SmootherType_PETSc_RBBJ) :: smoother
+      TYPE(ParamType) :: params
+      LOGICAL(SBK) :: tmpbool
+      INTEGER(SIK) :: num_red,num_black,istt,istp,blk_size
+      INTEGER(SIK) :: i
+      LOGICAL(SBK),ALLOCATABLE :: is_red(:)
+#ifdef FUTILITY_HAVE_PETSC
+      KSP :: ksp
+      PC :: pc
+      KSPType :: myksptype
+      PCType :: mypctype
+      PetscErrorCode :: iperr
+
+      CALL KSPCreate(mpiTestEnv%comm,ksp,iperr)
+      CALL KSPSetType(ksp,KSPGMRES,iperr)
+      CALL KSPGetPC(ksp,pc,iperr)
+      CALL PCSetType(pc,PCJACOBI,iperr)
+#else
+      INTEGER(SIK) :: ksp=-1_SIK
+      INTEGER(SIK) :: pc
+#endif
+      num_red=250
+      num_black=250
+      istt=1
+      istp=500
+      blk_size=1_SIK
+      ALLOCATE(is_red(istt:istp))
+      is_red=.FALSE.
+      DO i=istt,istp,2
+        is_red(i)=.TRUE.
+      ENDDO
+
+      CALL params%add('SmootherType->is_red',is_red)
+      CALL params%add('SmootherType->istt',istt)
+      CALL params%add('SmootherType->istp',istp)
+      CALL params%add('SmootherType->blk_size',blk_size)
+      CALL params%add('SmootherType->MPI_Comm_ID',mpiTestEnv%comm)
+      CALL smoother%init(ksp,params)
+      CALL params%clear()
+
+      tmpbool=ALLOCATED(smoother%is_red) .AND. &
+                LBOUND(smoother%is_red,DIM=1) == istt .AND. &
+                UBOUND(smoother%is_red,DIM=1) == istp
+      ASSERT(tmpbool,'Smoother%isred allocated with correct bounds')
+
+      tmpbool=smoother%num_red == num_red .AND. &
+                smoother%num_black == num_black .AND. &
+                smoother%istt == istt .AND. &
+                smoother%istp == istp .AND. &
+                smoother%blk_size == blk_size
+      ASSERT(tmpbool,'smoother parameters are the correct value')
+
+#ifdef FUTILITY_HAVE_PETSC
+      CALL KSPGetType(ksp,myksptype,iperr)
+      CALL PCGetType(pc,mypctype,iperr)
+      tmpbool=iperr == 0_SIK .AND. &
+              myksptype == KSPRICHARDSON .AND. &
+              mypctype == PCSHELL
+      ASSERT(tmpbool,'ksp and pc set to the correct types in PETSc')
+      CALL KSPGetType(smoother%ksp,myksptype,iperr)
+      CALL PCGetType(smoother%pc,mypctype,iperr)
+      tmpbool=iperr == 0_SIK .AND. &
+              myksptype == KSPRICHARDSON .AND. &
+              mypctype == PCSHELL
+      ASSERT(tmpbool,"smoother's copy of ksp and pc set to the correct types in PETSc")
+#endif
+
+      tmpbool=smoother%MPIparallelEnv%isInit()
+      ASSERT(tmpbool,'smoother ParEnv initialized.')
+
+      ASSERT(smoother%isInit,'Smoother  initialized')
+
+      CALL smoother%clear()
+      CALL KSPDestroy(ksp,iperr)
+
+    ENDSUBROUTINE testInit_PETSc_RBBJ
+!
+!-------------------------------------------------------------------------------
+    SUBROUTINE testInit_PETSc_RBBJ()
+      TYPE(SmootherType_PETSc_RBBJ) :: smoother
+      TYPE(ParamType) :: params
+      LOGICAL(SBK) :: tmpbool
+      INTEGER(SIK) :: num_red,num_black,istt,istp,blk_size
+      INTEGER(SIK) :: i
+      LOGICAL(SBK),ALLOCATABLE :: is_red(:)
+#ifdef FUTILITY_HAVE_PETSC
+      KSP :: ksp
+      PetscErrorCode :: iperr
+
+      CALL KSPCreate(mpiTestEnv%comm,ksp,iperr)
+#else
+      INTEGER(SIK) :: ksp=-1_SIK
+#endif
+
+      num_red=250
+      num_black=250
+      istt=1
+      istp=500
+      blk_size=1_SIK
+      ALLOCATE(is_red(istt:istp))
+      is_red=.FALSE.
+      DO i=istt,istp,2
+        is_red(i)=.TRUE.
+      ENDDO
+
+      CALL params%add('SmootherType->is_red',is_red)
+      CALL params%add('SmootherType->istt',istt)
+      CALL params%add('SmootherType->istp',istp)
+      CALL params%add('SmootherType->blk_size',blk_size)
+      CALL params%add('SmootherType->MPI_Comm_ID',mpiTestEnv%comm)
+      CALL smoother%init(ksp,params)
+      CALL params%clear()
+
+      tmpbool=ALLOCATED(smoother%is_red) .AND. &
+                LBOUND(smoother%is_red,DIM=1) == istt .AND. &
+                UBOUND(smoother%is_red,DIM=1) == istp
+      ASSERT(tmpbool,'Smoother%isred allocated with correct bounds')
+
+      tmpbool=smoother%num_red == num_red .AND. &
+                smoother%num_black == num_black .AND. &
+                smoother%istt == istt .AND. &
+                smoother%istp == istp .AND. &
+                smoother%blk_size == blk_size
+      ASSERT(tmpbool,'smoother parameters are the correct value')
+
+#ifdef FUTILITY_HAVE_PETSC
+      CALL KSPGetType(ksp,myksptype,iperr)
+      CALL PCGetType(pc,mypctype,iperr)
+      tmpbool=iperr == 0_SIK .AND. &
+              myksptype == KSPRICHARDSON .AND. &
+              mypctype == PCSHELL
+      ASSERT(tmpbool,'ksp and pc set to the correct types in PETSc')
+      CALL KSPGetType(smoother%ksp,myksptype,iperr)
+      CALL PCGetType(smoother%pc,mypctype,iperr)
+      tmpbool=iperr == 0_SIK .AND. &
+              myksptype == KSPRICHARDSON .AND. &
+              mypctype == PCSHELL
+      ASSERT(tmpbool,"smoother's copy of ksp and pc set to the correct types in PETSc")
+#endif
+
+      tmpbool=smoother%MPIparallelEnv%isInit()
+      ASSERT(tmpbool,'smoother ParEnv initialized.')
+
+      ASSERT(smoother%isInit,'Smoother  initialized')
+
+      CALL smoother%clear()
+      CALL KSPDestroy(ksp,iperr)
+
+    ENDSUBROUTINE testInit_PETSc_RBBJ
 
 ENDPROGRAM testSmoother
